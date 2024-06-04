90c94e140b667c20cf42500fffc010a544ee7c73
Niranjan <kneerunjun@gmail.com>
2024-06-04 15:21:36 +0530

    unittest workflow tested for environment variables

--- quick_test.go
+++ quick_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,8 +9,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/eensymachines-in/patio/aquacfg"
-	"github.com/eensymachines-in/webpi-telegnotify/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -33,36 +30,34 @@ func TestTelegGetMe(t *testing.T) {
 	t.Log(result)
 }
 
-func TestTelegSendMessage(t *testing.T) {
-	cl := &http.Client{Timeout: 5 * time.Second}
-	url := fmt.Sprintf("%s%s/sendMessage", os.Getenv("BOT_BASEURL"), os.Getenv("BOT_TOK"))
+// func TestTelegSendMessage(t *testing.T) {
+// 	cl := &http.Client{Timeout: 5 * time.Second}
+// 	url := fmt.Sprintf("%s%s/sendMessage", os.Getenv("BOT_BASEURL"), os.Getenv("BOT_TOK"))
 
-	msg, _ := models.Notification("Aquaponics pump-III", "D5-5C-1C-04-81-29", time.Now(), models.CfgChange(&aquacfg.Schedule{
-		Config:   2,
-		TickAt:   "14:00",
-		PulseGap: 100,
-		Interval: 500,
-	})).ToMessageTxt()
-	bm := BotMessage{ChatID: GRP_ID, Txt: msg, ParseMode: "markdown"}
-	// byt, _ := json.Marshal(map[string]string{"chat_id": GRP_ID, "text": "This is hi from inside the bot"})
-	byt, _ := json.Marshal(bm)
+// 	msg, _ := models.Notification("Aquaponics pump-III", "D5-5C-1C-04-81-29", time.Now(), models.CfgChange(&aquacfg.Schedule{
+// 		Config:   2,
+// 		TickAt:   "14:00",
+// 		PulseGap: 100,
+// 		Interval: 500,
+// 	})).ToMessageTxt()
+// 	bm := BotMessage{ChatID: GRP_ID, Txt: msg, ParseMode: "markdown"}
+// 	// byt, _ := json.Marshal(map[string]string{"chat_id": GRP_ID, "text": "This is hi from inside the bot"})
+// 	byt, _ := json.Marshal(bm)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(byt))
-	req.Header.Set("Content-Type", "application/json")
-	assert.Nil(t, err, "failed to create new request")
-	assert.NotNil(t, req, "Unexpected nil request object")
+// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(byt))
+// 	req.Header.Set("Content-Type", "application/json")
+// 	assert.Nil(t, err, "failed to create new request")
+// 	assert.NotNil(t, req, "Unexpected nil request object")
 
-	resp, err := cl.Do(req)
-	assert.Nil(t, err, "Unexpected error when sending request")
-	assert.NotNil(t, resp, "Unexpected nil response")
+// 	resp, err := cl.Do(req)
+// 	assert.Nil(t, err, "Unexpected error when sending request")
+// 	assert.NotNil(t, resp, "Unexpected nil response")
 
-	byt, err = io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	assert.Nil(t, err, "Unexpected error when reading response body")
-	result := map[string]interface{}{}
-	err = json.Unmarshal(byt, &result)
-	assert.Nil(t, err, "Unexpected error when unmarshalling")
-	t.Log(result)
-}
-
-
+// 	byt, err = io.ReadAll(resp.Body)
+// 	defer resp.Body.Close()
+// 	assert.Nil(t, err, "Unexpected error when reading response body")
+// 	result := map[string]interface{}{}
+// 	err = json.Unmarshal(byt, &result)
+// 	assert.Nil(t, err, "Unexpected error when unmarshalling")
+// 	t.Log(result)
+// }
