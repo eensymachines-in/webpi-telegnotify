48b1c2b897f7558f396f51481fd052ccf74a0594
Niranjan <kneerunjun@gmail.com>
2024-06-17 13:14:37 +0530

    ready for test and deployment

--- quick_test.go
+++ quick_test.go
@@ -18,20 +18,25 @@ import (
 func TestApi(t *testing.T) {
 	cl := &http.Client{Timeout: 5 * time.Second}
 	baseurl := "http://localhost:8080/api/devices/b8:27:eb:a5:be:48/notifications"
-
+	type payload struct {
+		Dttm         time.Time                `json:"dttm"`
+		Notification models.TelegNotification `json:"notification"`
+	}
 	t.Run("invalid_query_param", func(t *testing.T) {
 		url := fmt.Sprintf("%s/?typ=invalidparam", baseurl)
-		not := models.Notification("Test aquaponics configuration", "b8:27:eb:a5:be:48", time.Now(), models.CfgChange(&aquacfg.Schedule{
-			Config:   1,
-			TickAt:   "11:30",
-			PulseGap: 100,
-			Interval: 500,
-		}))
-
-		byt, err := json.Marshal(not)
+		pl := payload{
+			Dttm: time.Now(),
+			Notification: models.CfgChange(&aquacfg.Schedule{
+				Config:   1,
+				TickAt:   "11:30",
+				PulseGap: 100,
+				Interval: 500,
+			}),
+		}
+		byt, err := json.Marshal(pl)
 		assert.Nil(t, err, "Unexpected error when marshaling bot message")
-		payload := bytes.NewBuffer(byt)
-		req, err := http.NewRequest("POST", url, payload)
+		buff := bytes.NewBuffer(byt)
+		req, err := http.NewRequest("POST", url, buff)
 		assert.Nil(t, err, "Unexpected error when forming the request")
 		resp, err := cl.Do(req)
 		assert.Nil(t, err, "unexpected error when executing the request, do you have access to the server ?")
@@ -40,17 +45,20 @@ func TestApi(t *testing.T) {
 	// NOTE: there isnt a possibility of a bad configuration - since the update is tightly guarded by cfgwatch
 	t.Run("cfg_change", func(t *testing.T) {
 		url := fmt.Sprintf("%s/?typ=cfgchange", baseurl)
-		not := models.Notification("Test aquaponics configuration", "b8:27:eb:a5:be:48", time.Now(), models.CfgChange(&aquacfg.Schedule{
-			Config:   1,
-			TickAt:   "11:30",
-			PulseGap: 100,
-			Interval: 500,
-		}))
+		pl := payload{
+			Dttm: time.Now(),
+			Notification: models.CfgChange(&aquacfg.Schedule{
+				Config:   1,
+				TickAt:   "11:30",
+				PulseGap: 100,
+				Interval: 500,
+			}),
+		}
 
-		byt, err := json.Marshal(not)
+		byt, err := json.Marshal(pl)
 		assert.Nil(t, err, "Unexpected error when marshaling bot message")
-		payload := bytes.NewBuffer(byt)
-		req, err := http.NewRequest("POST", url, payload)
+		buff := bytes.NewBuffer(byt)
+		req, err := http.NewRequest("POST", url, buff)
 		assert.Nil(t, err, "Unexpected error when forming the request")
 		resp, err := cl.Do(req)
 		assert.Nil(t, err, "unexpected error when executing the request, do you have access to the server ?")
@@ -59,12 +67,14 @@ func TestApi(t *testing.T) {
 
 	t.Run("GPIO_report_status", func(t *testing.T) {
 		url := fmt.Sprintf("%s/?typ=gpiostat", baseurl)
-		not := models.Notification("Test aquaponics configuration", "b8:27:eb:a5:be:48", time.Now(), models.GpioStatus(models.PinStatus("Motor control relay", models.ACTUATOR, 33, models.DIGIPIN_HIGH)))
-
-		byt, err := json.Marshal(not)
+		pl := payload{
+			Dttm:         time.Now(),
+			Notification: models.GpioStatus(models.PinStatus("Motor control relay", models.ACTUATOR, 33, models.DIGIPIN_HIGH)),
+		}
+		byt, err := json.Marshal(pl)
 		assert.Nil(t, err, "Unexpected error when marshaling bot message")
-		payload := bytes.NewBuffer(byt)
-		req, err := http.NewRequest("POST", url, payload)
+		buff := bytes.NewBuffer(byt)
+		req, err := http.NewRequest("POST", url, buff)
 		assert.Nil(t, err, "Unexpected error when forming the request")
 		resp, err := cl.Do(req)
 		assert.Nil(t, err, "unexpected error when executing the request, do you have access to the server ?")
@@ -73,13 +83,15 @@ func TestApi(t *testing.T) {
 
 	t.Run("vital_status", func(t *testing.T) {
 		url := fmt.Sprintf("%s/?typ=vitals", baseurl)
-		not := models.Notification("Test aquaponics configuration", "b8:27:eb:a5:be:48", time.Now(), models.VitalStats("active", "active", "HTTP/2 200", "16 7", "4d 8h"))
-		t.Log("Now logging the vital stats notification")
-		t.Log(not.ToMessageTxt())
-		byt, err := json.Marshal(not)
+		pl := payload{
+			Dttm:         time.Now(),
+			Notification: models.VitalStats("active", "active", "HTTP/2 200", "16 7", "4d 8h"),
+		}
+
+		byt, err := json.Marshal(pl)
 		assert.Nil(t, err, "Unexpected error when marshaling bot message")
-		payload := bytes.NewBuffer(byt)
-		req, err := http.NewRequest("POST", url, payload)
+		buff := bytes.NewBuffer(byt)
+		req, err := http.NewRequest("POST", url, buff)
 		assert.Nil(t, err, "Unexpected error when forming the request")
 		resp, err := cl.Do(req)
 		assert.Nil(t, err, "unexpected error when executing the request, do you have access to the server ?")
@@ -104,35 +116,3 @@ func TestTelegGetMe(t *testing.T) {
 	assert.Nil(t, err, "Unexpected error when unmarshalling")
 	t.Log(result)
 }
-
-// func TestTelegSendMessage(t *testing.T) {
-// 	cl := &http.Client{Timeout: 5 * time.Second}
-// 	url := fmt.Sprintf("%s%s/sendMessage", os.Getenv("BOT_BASEURL"), os.Getenv("BOT_TOK"))
-
-// 	msg, _ := models.Notification("Aquaponics pump-III", "D5-5C-1C-04-81-29", time.Now(), models.CfgChange(&aquacfg.Schedule{
-// 		Config:   2,
-// 		TickAt:   "14:00",
-// 		PulseGap: 100,
-// 		Interval: 500,
-// 	})).ToMessageTxt()
-// 	bm := BotMessage{ChatID: GRP_ID, Txt: msg, ParseMode: "markdown"}
-// 	// byt, _ := json.Marshal(map[string]string{"chat_id": GRP_ID, "text": "This is hi from inside the bot"})
-// 	byt, _ := json.Marshal(bm)
-
-// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(byt))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	assert.Nil(t, err, "failed to create new request")
-// 	assert.NotNil(t, req, "Unexpected nil request object")
-
-// 	resp, err := cl.Do(req)
-// 	assert.Nil(t, err, "Unexpected error when sending request")
-// 	assert.NotNil(t, resp, "Unexpected nil response")
-
-// 	byt, err = io.ReadAll(resp.Body)
-// 	defer resp.Body.Close()
-// 	assert.Nil(t, err, "Unexpected error when reading response body")
-// 	result := map[string]interface{}{}
-// 	err = json.Unmarshal(byt, &result)
-// 	assert.Nil(t, err, "Unexpected error when unmarshalling")
-// 	t.Log(result)
-// }
